657c1a9aff326aa514ebdb3afb422c4a0bdc525a
kisekiG <dh568318869@gmail.com>
2023-04-05 10:48:41 +0800

    2023-03-29 learn java

--- src/Test17_Employee.java
+++ src/Test17_Employee.java
@@ -1,11 +1,13 @@
 public class Test17_Employee {
     public static void main(String[] args) {
-        Employee employee = new Employee("令狐冲", "9527", 2000);
-        employee.work();
+        Coder coder = new Coder("程序员", "007", 10000.64);
+        coder.work();
+        Manager manager = new Manager("项目经理", "001", 50000.99, 30000);
+        manager.work();
     }
 }
 
-class Employee {
+abstract class Employee {
     private String name, id;
     private double salary;
 
@@ -42,7 +44,43 @@ class Employee {
         this.salary = salary;
     }
 
+    public abstract void work();
+
+}
+
+class Coder extends Employee {
+    public Coder() {
+    }
+
+    public Coder(String name, String id, double salary) {
+        super(name, id, salary);
+    }
+
     public void work() {
-        System.out.println("我的姓名：" + name + "，我的工号是：" + id + "，我的工资是：" + salary + "，我的工作内容是敲代码");
+        System.out.println("我的姓名：" + this.getName() + "，我的工号是：" + this.getId() + "，我的工资是：" + this.getSalary() + "，我的工作内容是敲代码");
+    }
+}
+
+class Manager extends Employee {
+    private double bonus;
+
+    public Manager() {
+    }
+
+    public Manager(String name, String id, double salary, double bonus) {
+        super(name, id, salary);
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public void work() {
+        System.out.println("我的姓名：" + this.getName() + "，我的工号是：" + this.getId() + "，我的工资是：" + this.getSalary() + "，我的奖金是" + this.bonus + "，我的工作内容是管理项目");
     }
 }
